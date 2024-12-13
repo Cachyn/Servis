a9bf90b605dd6d807b7fc3437461c889bd46c9c3
Cachyn <cachyn@seznam.cz>
2024-12-13 22:46:25 +0100

    Oprava backendu

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,14 @@
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+});
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
